2b10e4f0bc97342e9a19ea8676be2ba5993c75ba
Jesse Krim <jesse.krim@gmail.com>
2019-10-16 14:06:44 -0400

    Update server/app.js to include login and logout routes, also update middleware

--- server/app.js
+++ server/app.js
@@ -5,95 +5,152 @@ const partials = require('express-partials');
 const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
 const models = require('./models');
+const morgan = require('morgan');
 
 const app = express();
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 app.use(partials());
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(Auth());
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
+app.get('/create', (req, res) => {
+  res.render('index');
+});
 
-app.get('/', 
-  (req, res) => {
-    res.render('index');
-  });
+app.get('/links', (req, res, next) => {
+  models.Links.getAll()
+    .then(links => {
+      res.status(200).send(links);
+    })
+    .error(error => {
+      res.status(500).send(error);
+    });
+});
 
-app.get('/create', 
-  (req, res) => {
-    res.render('index');
-  });
-
-app.get('/links', 
-  (req, res, next) => {
-    models.Links.getAll()
-      .then(links => {
-        res.status(200).send(links);
-      })
-      .error(error => {
-        res.status(500).send(error);
-      });
-  });
-
-app.post('/links', 
-  (req, res, next) => {
-    var url = req.body.url;
-    if (!models.Links.isValidUrl(url)) {
+app.post('/links', (req, res, next) => {
+  var url = req.body.url;
+  if (!models.Links.isValidUrl(url)) {
     // send back a 404 if link is not valid
-      return res.sendStatus(404);
-    }
+    return res.sendStatus(404);
+  }
 
-    return models.Links.get({ url })
-      .then(link => {
-        if (link) {
-          throw link;
-        }
-        return models.Links.getUrlTitle(url);
-      })
-      .then(title => {
-        return models.Links.create({
-          url: url,
-          title: title,
-          baseUrl: req.headers.origin
-        });
-      })
-      .then(results => {
-        return models.Links.get({ id: results.insertId });
-      })
-      .then(link => {
+  return models.Links.get({ url })
+    .then(link => {
+      if (link) {
         throw link;
-      })
-      .error(error => {
-        res.status(500).send(error);
-      })
-      .catch(link => {
-        res.status(200).send(link);
+      }
+      return models.Links.getUrlTitle(url);
+    })
+    .then(title => {
+      return models.Links.create({
+        url: url,
+        title: title,
+        baseUrl: req.headers.origin
       });
-  });
+    })
+    .then(results => {
+      return models.Links.get({ id: results.insertId });
+    })
+    .then(link => {
+      throw link;
+    })
+    .error(error => {
+      res.status(500).send(error);
+    })
+    .catch(link => {
+      res.status(200).send(link);
+    });
+});
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
 //setup signup and login routes
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
 app.post('/signup', (req, res, next) => {
-  console.log('TCL: req', req.body.username);
-  //first check if the user is already set up
-  //if not then create a new user
-  return models.Users.create({username, password})
-    .then(newUser =>{
-      return models.Model.create(newUser);
+  var username = req.body.username;
+  var password = req.body.password;
+
+  return models.Users.get({ username })
+    .then(user => {
+      if (user) {
+        // user already exists; throw user to catch and redirect
+        throw user;
+      }
+
+      return models.Users.create({ username, password });
     })
-    .catch(newUser => {
-      res.status(200).send(newUser);
+    .then(results => {
+      return models.Sessions.update(
+        { hash: req.session.hash },
+        { userId: results.insertId }
+      );
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .error(error => {
+      res.status(500).send(error);
+    })
+    .catch(user => {
+      res.redirect('/signup');
     });
 });
 
-
 //app.post('/login')
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
+app.post('/login', (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  return models.Users.get({ username })
+    .then(user => {
+      if (!user || !models.Users.compare(password, user.password, user.salt)) {
+        // user doesn't exist or the password doesn't match
+        throw new Error('Username and password do not match');
+      }
+
+      return models.Sessions.update(
+        { hash: req.session.hash },
+        { userId: user.id }
+      );
+    })
+    .then(() => {
+      res.redirect('/');
+    })
+    .error(error => {
+      res.status(500).send(error);
+    })
+    .catch(() => {
+      res.redirect('/login');
+    });
+});
+
+app.get('/logout', (req, res, next) => {
+  return models.Sessions.delete({ hash: req.cookies.shortlyid })
+    .then(() => {
+      res.clearCookie('shortlyid');
+      res.redirect('/login');
+    })
+    .error(error => {
+      res.status(500).send(error);
+    });
+});
 
 /************************************************************/
 // Handle the code parameter route last - if all other routes fail
@@ -102,10 +159,8 @@ app.post('/signup', (req, res, next) => {
 /************************************************************/
 
 app.get('/:code', (req, res, next) => {
-
   return models.Links.get({ code: req.params.code })
     .tap(link => {
-
       if (!link) {
         throw new Error('Link does not exist');
       }
